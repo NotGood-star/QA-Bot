f356623740a594f615db28d979ed42f70018830d
Not_Good <reetujoshi588@gmail.com>
2026-08-03 07:39:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 require("dotenv").config();
 
+const express = require("express");
 const { Client, GatewayIntentBits } = require("discord.js");
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Simple web server for Render
+app.get("/", (req, res) => {
+    res.send("QA Bot is online!");
+});
+
+app.listen(PORT, () => {
+    console.log(`🌐 Web server running on port ${PORT}`);
+});
+
+// Discord Bot
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds
@@ -9,7 +23,7 @@ const client = new Client({
 });
 
 client.once("ready", () => {
-    console.log(`✅ Logged in as ${client.user.tag}`);
+    console.log(`🤖 Logged in as ${client.user.tag}`);
 });
 
 client.login(process.env.TOKEN);
