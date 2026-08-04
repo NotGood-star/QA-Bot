ec60343a68b422ca92c79ea1f952d0c35900b6ba
Not_Good <reetujoshi588@gmail.com>
2026-08-04 21:31:56 +0530

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -4,21 +4,23 @@ module.exports = {
         if (interaction.isChatInputCommand()) {
             const command = client.commands.get(interaction.commandName);
             if (!command) return;
-
             try {
                 await command.execute(interaction, client);
             } catch (error) {
-                console.error(`Error executing ${interaction.commandName}:`, error);
-                if (interaction.replied || interaction.deferred) {
-                    await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-                } else {
-                    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+                console.error(error);
+                await interaction.reply({ content: 'Error executing command.', ephemeral: true }).catch(() => {});
+            }
+        } else if (interaction.isStringSelectMenu()) {
+            if (interaction.customId === 'request_test_menu') {
+                const testCommand = client.commands.get('test') || require('../commands/test');
+                if (testCommand.handleSelectMenu) {
+                    await testCommand.handleSelectMenu(interaction, client);
                 }
             }
         } else if (interaction.isModalSubmit()) {
             if (interaction.customId === 'test_create_modal') {
-                const testCommand = client.commands.get('test');
-                if (testCommand && testCommand.handleModal) {
+                const testCommand = client.commands.get('test') || require('../commands/test');
+                if (testCommand.handleModal) {
                     await testCommand.handleModal(interaction, client);
                 }
             }
